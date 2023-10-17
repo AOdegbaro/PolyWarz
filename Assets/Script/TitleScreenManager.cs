3e30a5c3e2e8dd40e7fc0dd6bf357b509b1c1429
RosyMoonVT <142060233+RosyMoonVT@users.noreply.github.com>
2023-10-16 21:00:15 -0500

    Creating the Main Menu Scene, World Scene, Title Screen Manager Script & World Save Manager Script - AO

--- Assets/Script/TitleScreenManager.cs
+++ Assets/Script/TitleScreenManager.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class TitleScreenManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartNetworkAsHost()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        NetworkManager.Singleton.StartHost();
     }
 }
