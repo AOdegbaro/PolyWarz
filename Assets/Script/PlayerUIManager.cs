b9f3b5d1628a420b2223abaa4a53a29cb75a7be9
RosyMoonVT <142060233+RosyMoonVT@users.noreply.github.com>
2023-10-16 21:59:40 -0500

    successfully connected to the network - AO

--- Assets/Script/PlayerUIManager.cs
+++ Assets/Script/PlayerUIManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class PlayerUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PlayerUIManager instance;
+    
+    [Header("NETWORK JOIN")] 
+    [SerializeField] bool startGameAsClient;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (startGameAsClient)
+        {
+            startGameAsClient = false;
+            // We must first shut down, because we have started as a host during the title screen 
+            NetworkManager.Singleton.Shutdown();
+            // We then restart, as a client 
+            NetworkManager.Singleton.StartClient();
+        }
     }
 }
